a43454657bf5e71ae398944cfc19a728f3a4f99e
Thanaphon Sarnsamuth <thanaphon.sar@axonstech.com>
2026-01-11 16:33:14 +0700

    Task2

--- src/repositories/EventRepositoryPrisma.ts
+++ src/repositories/EventRepositoryPrisma.ts
@@ -1,104 +1,107 @@
-import {prisma} from '../lib/prisma'
-import {Prisma} from "../generated/prisma/client";
-import type {eventModel as Event} from "../generated/prisma/models/event";
-import {PageEvent} from "../models/EventPage";
-
+import { prisma } from "../lib/prisma";
+import { Prisma } from "../generated/prisma/client";
+import type { eventModel as Event } from "../generated/prisma/models/event";
+import { PageEvent } from "../models/EventPage";
 
 export function getEventByCategory(category: string) {
-    return prisma.event.findMany({
-        where: {category},
-    });
+  return prisma.event.findMany({
+    where: { category },
+  });
 }
 
 export function getAllEvents() {
-    return prisma.event.findMany();
+  return prisma.event.findMany();
 }
 
 export function getEventById(id: number) {
-    return prisma.event.findUnique({
-        where: {id},
-        include: {organizer: true},
-        omit: {organizerId: true}
-    });
+  return prisma.event.findUnique({
+    where: { id },
+    include: { organizer: true },
+    omit: { organizerId: true },
+  });
 }
 
 export function addEvent(newEvent: Event) {
-    return prisma.event.create({
-        data: {
-            category: newEvent.category,
-            title: newEvent.title,
-            description: newEvent.description,
-            location: newEvent.location,
-            date: newEvent.date,
-            time: newEvent.time,
-            petsAllowed: newEvent.petsAllowed
-
-        }
-    });
+  return prisma.event.create({
+    data: {
+      category: newEvent.category,
+      title: newEvent.title,
+      description: newEvent.description,
+      location: newEvent.location,
+      date: newEvent.date,
+      time: newEvent.time,
+      petsAllowed: newEvent.petsAllowed,
+    },
+  });
 }
 
 export function getAllEventsWithOrganizer() {
-    return prisma.event.findMany({
+  return prisma.event.findMany({
+    select: {
+      id: true,
+      category: true,
+      title: true,
+      description: true,
+      location: true,
+      date: true,
+      time: true,
+      petsAllowed: true,
+      organizerId: false,
+      organizer: {
         select: {
-            id: true,
-            category: true,
-            organizerId: false,
-            organizer: {
-                select: {
-                    name: true
-                }
-            },
-            participants: {
-                select: {
-                    id: true,
-                    name: true,
-                    email: true,
-                    events: true
-                }
-            }
-        }
-    });
+          name: true,
+        },
+      },
+      // participants: {
+      //     select: {
+      //         id: true,
+      //         name: true,
+      //         email: true,
+      //         events: true
+      //     }
+      // }
+    },
+  });
 }
 
 export async function getAllEventsWithOrganizerPagination(
-    keyword: string,
-    pageSize: number,
-    pageNo: number,
+  keyword: string,
+  pageSize: number,
+  pageNo: number
 ) {
-    const where = {
-        OR: [
-            {
-                title: {
-                    contains: keyword,
-                    mode: Prisma.QueryMode.insensitive
-                }
-            },
-            {description: {contains: keyword}},
-            {category: {contains: keyword}},
-            {organizer: {name: {contains: keyword}}}
-
-        ]
-    }
-    const events = await prisma.event.findMany({
-        where,
-        skip: pageSize * (pageNo - 1),
-        take: pageSize,
+  const where = {
+    OR: [
+      {
+        title: {
+          contains: keyword,
+          mode: Prisma.QueryMode.insensitive,
+        },
+      },
+      { description: { contains: keyword } },
+      { category: { contains: keyword } },
+      { organizer: { name: { contains: keyword } } },
+    ],
+  };
+  const events = await prisma.event.findMany({
+    where,
+    skip: pageSize * (pageNo - 1),
+    take: pageSize,
+    select: {
+      id: true,
+      category: true,
+      title: true,
+      organizerId: false,
+      organizer: {
         select: {
-            id: true,
-            category: true,
-            title: true,
-            organizerId: false,
-            organizer: {
-                select: {
-                    name: true
-                }
-            }
-        }
-    });
-    const count = await prisma.event.count({where});
-    return {count, events} as unknown as PageEvent; //provide unknown because of type on return may not match with prisma type
+          name: true,
+        },
+      },
+    },
+  });
+  const count = await prisma.event.count({ where });
+  return { count, events } as unknown as PageEvent; //provide unknown because of type on return may not match with prisma type
 }
 
 export function countEvent() {
-    return prisma.event.count();
+  return prisma.event.count();
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from "express";
 import eventRoute from "./routes/EventRoute";
-const app = express()
-const port = 3000
-app.use(express.json())
-app.use('/events',eventRoute);
+import path from "path";
+import cors, { CorsOptions } from "cors";
+const app = express();
+const port = 3000;
+app.use(express.json());
+const corsOptions: CorsOptions = {
+  origin: ["http://localhost:5051"],
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
+
+app.use("/events", eventRoute);
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+const webApp = express();
+const webPort = 5050;
+webApp.use(express.static(path.join(process.cwd())));
+
+webApp.listen(webPort, () => {
+  console.log(`WebApp listening at http://localhost:${webPort}`);
+});
