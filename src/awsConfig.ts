cab0f8c316da5fb255d0fa0c79e1ec8670d18a18
Thanaphon Sarnsamuth <thanaphon.sar@axonstech.com>
2026-01-12 15:42:31 +0700

    Task6 part2

--- src/awsConfig.ts
+++ src/awsConfig.ts
@@ -1,13 +1,22 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { env } from "prisma/config";
+
+const accessKeyId = process.env.S3_ACCESS_KEY_ID;
+const secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
+const endpoint = process.env.S3_ENDPOINT;
+const region = process.env.S3_REGION;
+if (!accessKeyId || !secretAccessKey || !endpoint || !region) {
+  throw new Error(
+    "Missing required environment variables for AWS S3 configuration"
+  );
+}
 
 const s3Client = new S3Client({
   credentials: {
-    accessKeyId: env("S3_ACCESS_KEY_ID"),
-    secretAccessKey: env("S3_SECRET_ACCESS_KEY"),
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
   },
-  endpoint: env("S3_ENDPOINT"),
-  region: env("S3_REGION"),
+  endpoint: endpoint,
+  region: region,
   forcePathStyle: true,
 });
 export default s3Client;
